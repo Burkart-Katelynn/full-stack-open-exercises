acc64acbbf7fb6dff080e11601be91f845e7e981
Katelynn Burkart <katelynn.burkart@gmail.com>
2026-09-14 12:19:13 -0600

    Exercise 1.6

--- part1/src/App.tsx
+++ part1/src/App.tsx
@@ -1,69 +1,12 @@
-const Header = (props: { course: string }) => {
-  return <h1>{props.course}</h1>;
-}
-
-const Part = (props: { part: string; exercises: number }) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  );
-}
-
-type Part = { name: string;
-  exercises: number;
-};
-
-const Content = (props: { parts: Part[] }) => {
-  return (
-    <>
-      {props.parts.map((part) => (
-        <Part key={part.name} part={part.name} exercises={part.exercises} />
-      ))}
-    </>
-  );
-}
-
-const Total = (props: { parts: Part[] }) => {
-  const totalExercises = props.parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <p>
-      Number of exercises {totalExercises}
-    </p>
-  );
-} 
+import { useState } from "react";
 
 const App = () => {
- const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const name = course.name;
-  const parts = course.parts;
-
-
-  return (
-    <>
-      <Header course={name} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </>
-  );
-}
+  return <div>code here</div>;
+};
 
 export default App;
